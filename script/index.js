b6d73e54419a437de07a342c20b05f08f4db6059
rahalrazika <rahalraika@gmail.com>
2020-12-02 09:33:21 +0100

    add book function and add book to library

--- script/index.js
+++ script/index.js
@@ -1,9 +1,17 @@
 let myLibrary = [];
 
-function Book() {
-  // the constructor...
+function Book(title, author, numberOfPages, read) {
+ this.title = title
+ this.author = author
+ this.numberOfPages = numberOfPages
+ this.read = read
 }
 
-function addBookToLibrary() {
-  // do stuff here
+
+function addBookToLibrary(book) {
+myLibrary.push(book)
 }
+
+let book = new Book("my book", "jon doe", 240, true)
+addBookToLibrary(book)
+console.log(myLibrary)
