1ec7fe0076e0b512043235e256f68e949c85cd1f
Diego Lira <diegoalira@gmail.com>
2020-12-02 06:10:52 -0300

    Show books table

--- script/index.js
+++ script/index.js
@@ -1,17 +1,37 @@
 let myLibrary = [];
 
 function Book(title, author, numberOfPages, read) {
- this.title = title
- this.author = author
- this.numberOfPages = numberOfPages
- this.read = read
+    this.title = title
+    this.author = author
+    this.numberOfPages = numberOfPages
+    this.read = read
 }
 
 
 function addBookToLibrary(book) {
-myLibrary.push(book)
+    myLibrary.push(book)
 }
 
-let book = new Book("my book", "jon doe", 240, true)
+function showBooks(myLibrary) {
+    let tableBody = document.querySelector('.books-list')
+
+    for (let i = 0; i < myLibrary.length; i++) {
+        let row = `<tr> 
+        <td>${myLibrary[i].title} </td> 
+        <td>${myLibrary[i].author} </td>
+        <td>${myLibrary[i].numberOfPages} </td>
+        <td>${myLibrary[i].read} </td>
+        </tr>`
+
+        tableBody.innerHTML += row
+    }
+}
+
+let book = new Book("my book 1", "jon doe", 240, true)
+let book2 = new Book("my book 2", "jon doe", 340, true)
+let book3 = new Book("my book 3", "jon doe", 140, false)
+
 addBookToLibrary(book)
-console.log(myLibrary)
+addBookToLibrary(book2)
+addBookToLibrary(book3)
+showBooks(myLibrary)
